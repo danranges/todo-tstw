356a31eef77c29f2dad6ee1282d093914248b789
Dan Ranges <danranges@gmail.com>
2023-01-19 20:41:00 +0100

    Add header component mock-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-9xl ">Howdi</h1>
+    <div className="App h-screen  dark:bg-zinc-900">
+      <div className="flex flex-row justify-between px-4 py-2 dark:bg-zinc-900">
+        <h1 className="text-4xl tracking-tighter font-bold dark:text-white ">
+          AnElephant
+        </h1>
+        <div className="flex flex-col justify-end">
+          <ul className="flex flex-row font-semibold md:space-x-8 space-x-4 dark:text-white ">
+            <li>Home</li>
+            <li>Projects</li>
+            <li>Account</li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
