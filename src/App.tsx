3905ed24590ceb091427c5df25c42fd458cfb1db
Dan Ranges <danranges@gmail.com>
2023-01-21 21:01:26 +0100

    Mock-up Log In component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Landing from "./pages/Landing"
 import Nav from "./components/Nav"
 import CreateAccount from "./components/CreateAccount"
+import LogIn from "./components/LogIn"
 
 function App() {
   return (
     <div className="App flex flex-col justify-center items-center h-screen dark:bg-zinc-900">
-      <CreateAccount />
+      <LogIn />
     </div>
   )
 }
